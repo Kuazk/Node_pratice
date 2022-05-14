0964ef86ec07b3f1c869fbfbfbb517760fd5f930
Kuazk <allenz@M1-Mac-Allen.local>
2022-05-14 15:42:05 -0500

    update

---
+++ script.js
@@ -0,0 +1,9 @@
+// const script2 = require('./script2.js')
+
+import { largeNumber} from './script2.js';
+import fs from 'fs';
+const a = largeNumber;
+const b = 5;
+
+console.log(a,b); 
+
--- script.mjs
+++
@@ -1,7 +0,0 @@
-// const script2 = require('./script2.js')
-
-import { largeNumber} from './script2.mjs';
-const a = largeNumber;
-const b = 5;
-
-console.log(a,b);
