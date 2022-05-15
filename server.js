972758d175e7d25cb450c4a317b8b4385efc7d40
Kuazk <allenz@M1-Mac-Allen.local>
2022-05-14 21:52:50 -0500

    update

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 import express from 'express';
 
 const app = express();
-const port = 3000;
 
-app.get('/',(req,res) => {
-    res.send('get root')
-})
-app.get('/pro',(req,res) => {
-    res.send('get pro')
-})
-app.post('/pro',(req,res) => {
-    const user = {
-        name: 'allen'
-    }
-    res.send(user);
-})
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+
+app.get('/:id',(req,res) => {
+    console.log(req.params)
+    res.status(404).send("your mom");
+});
+
+app.listen(3000);
